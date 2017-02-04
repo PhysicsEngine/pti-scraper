13bfdc9d6318412a2272bb688e1469780c802866
Yohei Yamaguchi <joker13meister@gmail.com>
2017-02-04 19:25:00 +0900

    Update connection.

--- web-scraping/writer.py
+++ web-scraping/writer.py
@@ -11,10 +11,14 @@ class Writer(object):
     SAVE_PATH = "/var/pti/scrape/{0}.txt"
 
     def __init__(self):
-        user = os.environ.get('PTI_USER')
-        password = os.environ.get('PTI_PASSWORD')
-        host = os.environ.get('PTI_HOST')
-        database = os.environ.get('PTI_DB')
+#        user = os.environ.get('pti_user')
+#        password = os.environ.get('pti_password')
+#        host = os.environ.get('pti_host')
+#        database = os.environ.get('pti_db')
+        user = "pti"
+        password = "gussan_gologo13"
+        host = "localhost"
+        database = "pti"
         # djb.set_character_set('utf8')
 
         self.conn = pymysql.connect(user=user, password=password, host=host, database=database)
@@ -34,7 +38,7 @@ class Writer(object):
                 cur.execute('SET CHARACTER SET utf8;')
                 cur.execute('SET character_set_connection=utf8;')
                 sql = "INSERT INTO articles_articles(pub_date, url, author_id, title) VALUES (%s, %s, %s, %s)"
-                r = cur.execute(sql, (content.pub_date, content.url, content.author_id, content.title))
+                r = cur.execute(sql, (content.pub_date, content.url, content.author_id, "dummy title"))
                 ret = cursor.lastrowid
                 self.conn.commit()
         finally:
