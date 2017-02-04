3a4b9954dd95ccce4525a1d0eeb4ea082b3b0363
shoe116 <shupoppo.116@gmail.com>
2017-02-04 16:34:46 +0900

    add columns scraper

---
+++ web-scraping/reuters_jp_columns_scraper.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from scraping_lib import ScrapingLib
+import re
+class ReutersJpColummsScraper(object):
+  BASE_URL = "http://jp.reuters.com/news/archive/jp_column?view=page&page=1"
+  LOG_PATH = "/tmp/ReutersJpColummsScraper.log"
+  ARTICLE = "article"
+  RE_ARTICLE = re.compile(ARTICLE)
+
+
+  def __init__(self, log_path):
+    self.scraping = ScrapingLib(log_path)
+
+  @classmethod
+  def get_target_url(cls, page):
+    return cls.BASE_URL.format(page)
+
+  def get_sorp(self, page):
+    url = self.get_target_url(page)
+    return self.scraping.get_sorp(url, ScrapingLib.TYPE_REQUESTS)
+
+  def get_url_list(self, page):
+    href_list = self.get_sorp(page).find_all("a", href=self.RE_ARTICLE)
+    return map(lambda x: x.get("href"), href_list)
+
+
+## test
+if __name__ == '__main__':
+  scraper = ReutersJpColummsScraper(ReutersJpColummsScraper.LOG_PATH)
+  print scraper.get_url_list(1)
