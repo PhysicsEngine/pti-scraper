c9c855e325a471fcb6eacc800d1f8c66cd953c34
shoe116 <shupoppo.116@gmail.com>
2017-02-04 22:02:25 +0900

    bug fix

--- web-scraping/author.py
+++ web-scraping/author.py
@@ -3,7 +3,7 @@
 from db_uploader import DbUploader
 class Author(object):
 
-  def __init__(self, conn):
+  def __init__(self, name, conn):
     self.name = name
     self.db_uploader = DbUploader(conn)
     self.author_id = self._set_author_id()
@@ -18,8 +18,8 @@ class Author(object):
     return self.author_id
 
   def _set_author_id(self):
-    author_id = self.db_uploader.select_articles_authors(name)
+    author_id = self.db_uploader.select_articles_authors(self.name)
     if author_id == None:
-      return self.db_uploader.insert_articles_authors(name)
+      return self.db_uploader.insert_articles_authors(self.name)
    
     return author_id
