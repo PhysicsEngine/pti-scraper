03cea74f8bb119770e00139be17d6b97f058914d
shoe116 <shupoppo.116@gmail.com>
2017-02-04 14:16:54 +0900

    for js

--- web-scraping/reuters_the_wire_scraper.py
+++ web-scraping/reuters_the_wire_scraper.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from scraping_lib import ScrapingLib
+import re
 
 class ReutersTheWireScraper(object):
   LIST_URL = "http://jp.reuters.com/theWire"
-  ARTICLE_URL = "http://jp.reuters.com/article/{0}?sp=true"
+  ARTICLE = "article"
+  ARTICLE_URL = "http://jp.reuters.com/article/"
+  RE_ARTICLE = re.compile(ARTICLE)
   LOG_PATH = "/tmp/ReutersTheWireScraper.log"
+  ARTICLE_PARAM = "?sp=true"
   
   def __init__(self, log_path):
       self.scraping = ScrapingLib(log_path)
@@ -14,10 +18,10 @@ class ReutersTheWireScraper(object):
   def get_target_url(cls, article_url):
       return cls.BASE_URL.format(article_url)
   
-  def get_list_url(self, type):
-    return self.scraping.get_sorp(self.LIST_URL, type)
+  def get_url_list(self):
+    return self.scraping.get_sorp(self.LIST_URL, ScrapingLib.TYPE_DRIVER).find_all("a")
   
 ## test
 if __name__ == '__main__':
   scraper = ReutersTheWireScraper(ReutersTheWireScraper.LOG_PATH)
-  print scraper.get_list_url("")
+  print scraper.get_url_list()
