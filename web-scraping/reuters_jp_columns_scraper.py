4a5ca0e5f68167fc73151e183ac5a80ad4dd2c38
shoe116 <shupoppo.116@gmail.com>
2017-02-04 21:58:58 +0900

    mod reuters_jp_columns app

--- web-scraping/reuters_jp_columns_scraper.py
+++ web-scraping/reuters_jp_columns_scraper.py
@@ -22,12 +22,7 @@ class ReutersJpColummsScraper(object):
     url = self.get_target_url(page)
     return self.scraping.get_sorp(url, ScrapingLib.TYPE_REQUESTS)
 
-  def get_url_list(self, page):
-    href_list = self.get_sorp(page).find_all("a", href=self.RE_ARTICLE)
-    return map(lambda x: x.get("href"), href_list)
-
-
 ## test
 if __name__ == '__main__':
   scraper = ReutersJpColummsScraper(ReutersJpColummsScraper.LOG_PATH)
-  print scraper.get_url_list(2)
+  print scraper.get_sorp(2)
